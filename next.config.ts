6a0038a637b21a0598a14535b02366974c542e0d
dev-frankie <dev.frankie93@gmail.com>
2025-05-23 00:23:30 +0900

    env 분기

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
+import { config as loadDotenvFlow } from 'dotenv-flow'
+
+loadDotenvFlow({
+  node_env: process.env.NODE_ENV || 'development',
+})
 
 const nextConfig: NextConfig = {
   images: {
     domains: [
       'mixit-local.s3.ap-northeast-2.amazonaws.com',
-      // 다른 이미지 도메인도 필요시 추가
     ],
   },
-  /* config options here */
-};
+}
 
-export default nextConfig;
+export default nextConfig
